c85836bbc706629c68b047e91e14602593012f3a
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-08 19:05:43 +0200

    feat: aggiunge Configurazione DbContext

---
+++ App_Start/DbContextConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace dipendenti_management.App_Start
+{
+    public class DbContextConfig : DbContext
+    {
+
+        public DbContextConfig() : base("name=db_locale")
+        {
+        }
+    }
+}
