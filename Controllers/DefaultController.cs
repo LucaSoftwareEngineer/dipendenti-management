e631587bbc70eac256e22b46236dc7e6901c93bb
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-08 20:10:30 +0200

    feat: aggiunge controller di default che implementa l'action Index che ritorna la vista Index

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dipendenti_management.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewData["titolo"] = "Tool Gestione Dipendenti";
+            return View();
+        }
+    }
+}
